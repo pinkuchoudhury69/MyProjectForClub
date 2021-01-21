fab2d0d621647ca408d2b9ea24af14f8854ead5c
Kranti Kumar Choudhury <69806393+pinkuchoudhury69@users.noreply.github.com>
2021-01-21 20:18:19 +0530

    Add files via upload

---
+++ JarvisAi.py
@@ -0,0 +1,69 @@
+import speech_recognition as sr
+import pyttsx3
+import pywhatkit
+import datetime
+import wikipedia
+import pyjokes
+
+listener = sr.Recognizer()
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[1].id)
+
+
+def talk(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        talk("Good Morning!")
+
+    elif hour>=12 and hour<18:
+        talk("Good Afternoon!")
+
+    else:
+        talk("Good Evening!")
+
+    talk("I am Your virtual assistant. Please tell me how may I help you")
+def take_command():
+    try:
+        with sr.Microphone() as source:
+            print('listening...')
+            voice = listener.listen(source)
+            command = listener.recognize_google(voice)
+            command = command.lower()
+            if 'vai' in command:
+                command = command.replace('vai', '')
+                print(command)
+    except:
+        pass
+    return command
+
+
+def run_alexa():
+    command = take_command()
+    print(command)
+    if 'play' in command:
+        song = command.replace('play', '')
+        talk('playing ' + song)
+        pywhatkit.playonyt(song)
+    elif 'time' in command:
+        time = datetime.datetime.now().strftime('%I:%M %p')
+        talk('Current time is ' + time)
+        print(time)
+    elif 'who' in command:
+        person = command.replace('who', '')
+        info = wikipedia.summary(person, 1)
+        print(info)
+        talk(info)
+    elif 'joke' in command:
+        talk(pyjokes.get_joke())
+    else:
+        talk('Unable to Hear.Plz repeat.')
+
+
+while True:
+    wishMe()
+    run_alexa()
